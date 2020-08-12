e5c88d5462400dbee8e2c7707baf7b9bd8c867c8
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:24:50 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
