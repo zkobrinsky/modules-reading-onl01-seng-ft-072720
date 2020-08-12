97ab04e34284572efa6fa1971367945eb7725eb7
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:17:57 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,12 @@
+equire_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
+
+class FancyDance
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
