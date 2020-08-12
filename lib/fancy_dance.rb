74b691ad02cd46e8da195e79a34632163819ea53
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:27:06 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -4,11 +4,11 @@ require_relative './class_methods_module.rb'
 
 module FancyDance
 module InstanceMethods
-  
+
   def twirl
     "I'm twirling!"
   end
-  
+
   def jump
     "Look how high I'm jumping!"
   end
