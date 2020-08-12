fea43483fff1301ad6b9806bc0dcafb60aabc24f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:27:03 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,12 +1,31 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class FancyDance
-  include Dance
 
-  attr_accessor :name
+module FancyDance
+module InstanceMethods
+  
+  def twirl
+    "I'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumping!"
+  end
 
-  def initialize(name)
-    @name = name
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end
+
+module ClassMethods
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+end 
