a7df05e94357603f16a82f7ebf0a549fa1b64219
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:15:22 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -4,11 +4,12 @@ module MetaDancing
     "This class produces objects that love to dance."
   end
 
-  class Dancer
-  extend MetaDancing
 end
-class Kid
+
+class Dancer
   extend MetaDancing
 end
 
+class Kid
+  extend MetaDancing
 end
